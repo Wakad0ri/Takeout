97433284bd9014ccca20c9911b0c21881cdf4648
Wakad0ri <3081957957@qq.com>
2025-07-09 22:18:01 +0800

    更改

---
+++ sky_server/src/main/java/com/atguigu/TakeoutApplication.java
@@ -0,0 +1,9 @@
+package com.atguigu;
+
+@SpringBootApplication
+public class TakeoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeoutApplication.class, args);
+    }
+}
