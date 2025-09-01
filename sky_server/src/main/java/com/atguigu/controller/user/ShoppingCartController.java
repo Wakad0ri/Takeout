f4c835feee5ff6c2d4d22cc396ca85a0de4bb184
Wakad0ri <3081957957@qq.com>
2025-09-01 11:00:40 +0800

    订单功能完善（百度地图API）

---
+++ sky_server/src/main/java/com/atguigu/controller/user/ShoppingCartController.java
@@ -0,0 +1,37 @@
+package com.atguigu.controller.user;
+
+import com.atguigu.DTO.ShoppingCartType.ShoppingCartDTO;
+import com.atguigu.result.Result;
+import com.atguigu.service.ShoppingCartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/shoppingCart")
+@Slf4j
+@Tag(name = "用户端-购物车管理")
+public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     * @return result<String>
+     */
+    @PostMapping("/add")
+    @Operation(summary = "添加购物车")
+    public Result<String> add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
+        log.info("添加购物车：{}", shoppingCartDTO);
+        shoppingCartService.addShoppingCart(shoppingCartDTO);
+        return Result.success();
+    }
+
+}
